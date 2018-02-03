a97ce3691ad0322210614ecc02d3018c5d71151b
Shiori Hironaka <noahluces@gmail.com>
2018-02-04 07:27:15 +0900

    Frequencerをテストが通るように修正

--- s4/t000003/Frequencer.java
+++ s4/t000003/Frequencer.java
@@ -2,40 +2,52 @@ package s4.t000003; // Please modify to s4.Bnnnnnn, where nnnnnn is your student
 import java.lang.*;
 import s4.specification.*;
 
-/*
-   interface FrequencerInterface {     // This interface provides the design for frequency counter.
-   void setTarget(byte[]  target); // set the data to search.
-   void setSpace(byte[]  space);  // set the data to be searched target from.
-   int frequency(); //It return -1, when TARGET is not set or TARGET's length is zero
-//Otherwise, it return 0, when SPACE is not set or Space's length is zero
-//Otherwise, get the frequency of TAGET in SPACE
-int subByteFrequency(int start, int end);
-// get the frequency of subByte of taget, i.e target[start], taget[start+1], ... , target[end-1].
-// For the incorrect value of START or END, the behavior is undefined.
-*/
 
-
-public class Frequencer implements FrequencerInterface{
+public class Frequencer implements FrequencerInterface {
     // Code to Test, *warning: This code  contains intentional problem*
     byte [] myTarget;
     byte [] mySpace;
-    public void setTarget(byte [] target) { myTarget = target;}
-    public void setSpace(byte []space) { mySpace = space; }
+
+    @Override
+    public void setTarget(byte[] target) {
+        myTarget = target;
+    }
+
+    @Override
+    public void setSpace(byte[] space) {
+        mySpace = space;
+    }
+
+    @Override
     public int frequency() {
+        if(this.myTarget == null || this.myTarget.length == 0){
+            return -1;
+        }
+        if(this.mySpace == null || this.mySpace.length == 0){
+            return 0;
+        }
+
         int targetLength = myTarget.length;
         int spaceLength = mySpace.length;
+        // mySpaceの中からmyTargetの出現頻度を数える
         int count = 0;
-        for(int start = 0; start<spaceLength; start++) { // Is it OK?
+        for(int start = 0; start < spaceLength-targetLength; start++) { // Is it OK?
             boolean abort = false;
             for(int i = 0; i<targetLength; i++) {
-                if(myTarget[i] != mySpace[start+i]) { abort = true; break; }
+                if(myTarget[i] != mySpace[start+i]) {
+                    abort = true;
+                    break;
+                }
             }
-            if(abort == false) { count++; }
+            if(abort == false) {
+                count++;
+            }
         }
         return count;
     }
 
     // I know that here is a potential problem in the declaration.
+    @Override
     public int subByteFrequency(int start, int length) { 
         // Not yet, but it is not currently used by anyone.
         return -1;
@@ -57,5 +69,4 @@ public class Frequencer implements FrequencerInterface{
             System.out.println("Exception occurred: STOP");
         }
     }
-}	    
-
+}
