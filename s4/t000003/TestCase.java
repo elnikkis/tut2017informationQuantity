1bc9e1ff5a9bc69c7ca858b36397c58383e861bc
Shiori Hironaka <noahluces@gmail.com>
2018-02-04 05:13:55 +0900

    Add testcace for FrequencerInterface

--- s4/t000003/TestCase.java
+++ s4/t000003/TestCase.java
@@ -2,73 +2,137 @@ package s4.t000003; // Please modify to s4.Bnnnnnn, where nnnnnn is your student
 import java.lang.*;
 import s4.specification.*;
 
-/*
-interface FrequencerInterface {     // This interface provides the design for frequency counter.
-    void setTarget(byte[]  target); // set the data to search.
-    void setSpace(byte[]  space);  // set the data to be searched target from.
-    int frequency(); //It return -1, when TARGET is not set or TARGET's length is zero
-                    //Otherwise, it return 0, when SPACE is not set or Space's length is zero
-                    //Otherwise, get the frequency of TAGET in SPACE
-    int subByteFrequency(int start, int end);
-    // get the frequency of subByte of taget, i.e target[start], taget[start+1], ... , target[end-1].
-    // For the incorrect value of START or END, the behavior is undefined.
-}
-*/
-
-/*
-package s4.specification;
-public interface InformationEstimatorInterface{
-    void setTarget(byte target[]); // set the data for computing the information quantities
-    void setSpace(byte space[]); // set data for sample space to computer probability
-    double estimation(); // It returns 0.0 when the target is not set or Target's length is zero;
-// It returns Double.MAX_VALUE, when the true value is infinite, or space is not set.
-// The behavior is undefined, if the true value is finete but larger than Double.MAX_VALUE.
-// Note that this happens only when the space is unreasonably large. We will encounter other problem anyway.
-// Otherwise, estimation of information quantity, 
-}                        
-*/
-
 
 public class TestCase {
-    public static void main(String[] args) {
-	try {
-	    FrequencerInterface  myObject;
-	    int freq;
-	    System.out.println("checking s4.t000003.Frequencer");
-	    myObject = new s4.t000003.Frequencer();
-	    myObject.setSpace("Hi Ho Hi Ho".getBytes());
-	    myObject.setTarget("H".getBytes());
-	    freq = myObject.frequency();
-	    System.out.print("\"H\" in \"Hi Ho Hi Ho\" appears "+freq+" times. ");
-	    if(4 == freq) { System.out.println("OK"); } else {System.out.println("WRONG"); }
-	}
-	catch(Exception e) {
-	    System.out.println("Exception occurred: STOP");
-	}
+    private static int testCount = 0;
 
-	try {
-	    InformationEstimatorInterface myObject;
-	    double value;
-	    System.out.println("checking s4.t000003.InformationEstimator");
-	    myObject = new s4.t000003.InformationEstimator();
-	    myObject.setSpace("3210321001230123".getBytes());
-	    myObject.setTarget("0".getBytes());
-	    value = myObject.estimation();
-	    System.out.println(">0 "+value);
-	    myObject.setTarget("01".getBytes());
-	    value = myObject.estimation();
-	    System.out.println(">01 "+value);
-	    myObject.setTarget("0123".getBytes());
-	    value = myObject.estimation();
-	    System.out.println(">0123 "+value);
-	    myObject.setTarget("00".getBytes());
-	    value = myObject.estimation();
-	    System.out.println(">00 "+value);
-	}
-	catch(Exception e) {
-	    System.out.println("Exception occurred: STOP");
-	}
-
+    /** Test and print message.
+     *
+     * @param expr : boolean value
+     * @param message : String Message printed when failed
+     */
+    static void assertTrue(boolean expr, String message) {
+        testCount++;
+        System.out.printf("Test%2d ", testCount);
+        if(!expr) {
+            System.out.println("Failed: " + message);
+        }
+        else {
+            System.out.println("Passed");
+        }
     }
-}	    
-	    
+
+    /**
+     * Test frequency method of FrequencerInterface
+    **/
+    private static void testFrequency() {
+        {
+            FrequencerInterface frequencer = new Frequencer();
+            frequencer.setSpace("Foo Bar Foo Bar".getBytes());
+            frequencer.setTarget("Foo".getBytes());
+            int count = frequencer.frequency();
+            assertTrue(count == 2, "Foo appears 2 times");
+        }
+        {
+            FrequencerInterface frq = new Frequencer();
+            int ret = frq.frequency();
+            assertTrue(ret == 0, "When SPACE is not set, frequency should return 0.");
+        }
+        {
+            FrequencerInterface frq = new Frequencer();
+            frq.setSpace("".getBytes());
+            int ret = frq.frequency();
+            assertTrue(ret == 0, "When SPACE's length is zero, frequency should return 0.");
+        }
+        {
+            FrequencerInterface frq = new Frequencer();
+            frq.setSpace("foo bar foo ba".getBytes());
+            int ret = frq.frequency();
+            assertTrue(ret == -1, "When TARGET is not set, frequency should return -1.");
+        }
+        {
+            FrequencerInterface frq = new Frequencer();
+            frq.setSpace("foo bar foo ba".getBytes());
+            frq.setTarget("".getBytes());
+            int ret = frq.frequency();
+            assertTrue(ret == -1, "When TARGET's length is zero, frequency should return -1.");
+        }
+        {
+            FrequencerInterface frq = new Frequencer();
+            frq.setSpace("foo bar foo ba".getBytes());
+            frq.setTarget("o".getBytes());
+            int ret = frq.frequency();
+            assertTrue(ret == 4, "should return the frequency");
+        }
+    }
+
+    private static void testSubByteFrequency() {
+        {
+            FrequencerInterface frq = new Frequencer();
+            frq.setSpace("aaaaaaaaaaaaaaaaaaaaaaa".getBytes());
+            frq.setTarget("a".getBytes());
+            {
+                int ret = frq.subByteFrequency(0, 5);
+                assertTrue(ret == 5, "subByteFrequency 5");
+            }
+            {
+                int ret = frq.subByteFrequency(0, 2);
+                assertTrue(ret == 2, "subByteFrequency 2");
+            }
+            {
+                int ret = frq.subByteFrequency(0, 0);
+                assertTrue(ret == 2, "subByteFrequency 0");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            FrequencerInterface  myObject;
+            int freq;
+            System.out.println("checking s4.t000003.Frequencer");
+            myObject = new s4.t000003.Frequencer();
+            myObject.setSpace("Hi Ho Hi Ho".getBytes());
+            myObject.setTarget("H".getBytes());
+            freq = myObject.frequency();
+            System.out.print("\"H\" in \"Hi Ho Hi Ho\" appears "+freq+" times. ");
+            if(4 == freq) { System.out.println("OK"); } else {System.out.println("WRONG"); }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            System.out.println("Exception occurred: STOP");
+        }
+
+        try {
+            InformationEstimatorInterface myObject;
+            double value;
+            System.out.println("checking s4.t000003.InformationEstimator");
+            myObject = new s4.t000003.InformationEstimator();
+            myObject.setSpace("3210321001230123".getBytes());
+            myObject.setTarget("0".getBytes());
+            value = myObject.estimation();
+            System.out.println(">0 "+value);
+            myObject.setTarget("01".getBytes());
+            value = myObject.estimation();
+            System.out.println(">01 "+value);
+            myObject.setTarget("0123".getBytes());
+            value = myObject.estimation();
+            System.out.println(">0123 "+value);
+            myObject.setTarget("00".getBytes());
+            value = myObject.estimation();
+            System.out.println(">00 "+value);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            System.out.println("Exception occurred: STOP");
+        }
+
+        try {
+            testFrequency();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            System.out.println("Exception occurred: STOP");
+        }
+    }
+}
